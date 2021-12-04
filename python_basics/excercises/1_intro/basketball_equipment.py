4694ed8fbbfb6f38eabbf27175fe7eac9ad0edeb
hubenk <92019100+hubenk@users.noreply.github.com>
2021-12-04 13:30:32 +0200

    Add files via upload

---
+++ python_basics/excercises/1_intro/basketball_equipment.py
@@ -0,0 +1,16 @@
+annual_tax = int(input())
+
+shoes_price = annual_tax * 0.6
+
+tracksuit_price = shoes_price * 0.8
+
+ball_price = tracksuit_price * 1/4
+
+accessoaries_price = ball_price * 1/5
+
+tottal_expediture = annual_tax + shoes_price + tracksuit_price + ball_price + accessoaries_price
+
+print(tottal_expediture)
+
+
+
