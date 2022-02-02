383db1dfed52d75ed49ce04869bec426ddec2b3f
hubenk <92019100+hubenk@users.noreply.github.com>
2022-02-03 01:30:06 +0200

    Update tic_tac_toe2.py

--- python_fundamentals/more/3_list/tic_tac_toe2.py
+++ python_fundamentals/more/3_list/tic_tac_toe2.py
@@ -1,20 +1,42 @@
-first_player = 0
-second_player = 0
-
+winner = ""
+rows = []
 
 for x in range(3):
-    line = input().split()
-    for move in line:
-        if move == 0:
-            pass
-        elif move == "1":
-            first_player += 1
-        elif move == "2":
-            second_player += 1
+    line = input().split(" ")
+    rows.append(line)
 
-if first_player > second_player:
+for player in range(1, 3):
+    player = str(player)
+
+    if player == rows[0][0] and player == rows[0][1] and player == rows[0][2]:
+        winner = player
+        break
+    elif player == rows[1][0] and player == rows[1][1] and player == rows[1][2]:
+        winner = player
+        break
+    elif player == rows[2][0] and player == rows[2][1] and player == rows[2][2]:
+        winner = player
+        break
+    elif player == rows[0][0] and player == rows[1][0] and player == rows[2][0]:
+        winner = player
+        break
+    elif player == rows[0][1] and player == rows[1][1] and player == rows[2][1]:
+        winner = player
+        break
+    elif player == rows[0][2] and player == rows[1][2] and player == rows[2][2]:
+        winner = player
+        break
+    elif player == rows[0][0] and player == rows[1][1] and player == rows[2][2]:
+        winner = player
+        break
+    elif player == rows[0][2] and player == rows[1][1] and player == rows[2][0]:
+        winner = player
+        break
+
+if winner == "1":
     print("First player won")
-elif first_player < second_player:
+elif winner == "2":
     print("Second player won")
 else:
     print("Draw!")
+
