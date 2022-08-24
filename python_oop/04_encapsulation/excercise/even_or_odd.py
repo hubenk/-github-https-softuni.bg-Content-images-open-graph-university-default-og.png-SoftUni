d56081fd2c2723a5e7ec1bbfba353f33a1475eb2
hubenk <92019100+hubenk@users.noreply.github.com>
2022-08-24 10:43:34 +0300

    Delete even_or_odd.py

--- python_oop/04_encapsulation/excercise/even_or_odd.py
+++
@@ -1,9 +0,0 @@
-def even_odd(*args):
-    command = args[-1]
-    odd_even_check = 0 if command == "even" else 1
-
-    return [x for x in args[:-1] if x % 2 == odd_even_check]
-
-
-print(even_odd(1, 2, 3, 4, 5, 6, "even"))
-print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
