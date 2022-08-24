476e9df6623fda644fb43ec2f98b88a3bf699209
hubenk <92019100+hubenk@users.noreply.github.com>
2022-08-24 10:43:28 +0300

    Delete concatenate.py

--- python_oop/04_encapsulation/excercise/concatenate.py
+++
@@ -1,9 +0,0 @@
-def concatenate(*args):
-    result = ""
-    for el in args:
-        result += el
-    return result
-
-
-print(concatenate("Soft", "Uni", "Is", "Great", "!"))
-print(concatenate("I", " ", "Love", " ", "Python"))
